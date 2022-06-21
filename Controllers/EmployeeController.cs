24da60302ac7b61aa7fceb38ff676cfd1c0d51fc
surajsoni1707 <105903958+surajsoni1707@users.noreply.github.com>
2022-06-21 16:12:38 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using SampleMVCApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleMVCApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult GetAllEmployee()
+        {
+            List<Employee> empList = new List<Employee>()
+        {
+            new Employee{  Id = 1, Name = "Suraj", Department = "IT", Salary = 100000.1 },
+            new Employee{  Id = 2, Name = "Aman", Department = "Finance", Salary = 300000.1 },
+            new Employee{  Id = 3, Name = "Raju", Department = "Hr", Salary = 220000.1 },
+            new Employee{  Id = 4, Name = "ihil", Department = "IT", Salary = 310000.1 },
+        };
+            ViewBag.EmpList = empList;
+            return View();
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SampleMVCApp.Models;
+
+namespace SampleMVCApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult GetAllProduct()
+        {
+            List<Product> productList1 = new List<Product>()
+            {
+                new Product{Id=1,Name="Abc",Company="Comp1",Price=10.1 },
+                new Product{Id=2,Name="Pqr",Company="Comp2",Price=100.1 },
+                new Product{Id=3,Name="Xyz",Company="Comp3",Price=130.1 },
+                new Product{Id=4,Name="MC",Company="Comp4",Price=140.1 }
+            };
+            ViewBag.ProductList = productList1;
+            return View();
+        }
+    }
+}
